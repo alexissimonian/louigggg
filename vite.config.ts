8b13e2b218d19c3e882172c1dcc289700b0d6d75
Alexis <alexis.simonian@outlook.com>
2025-10-29 15:01:16 +0000

    added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    allowedHosts: ['dev.alexissimonian.net', 'gregoryraconte.com']
+    allowedHosts: ['dev.alexissimonian.net']
   },
   css: {
     preprocessorOptions: {
